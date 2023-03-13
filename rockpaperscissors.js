a924efd9ba46263766b76690043eae4e42f4df75
Justin Leong <124329491+JustinLeongHK@users.noreply.github.com>
2023-03-13 12:06:45 +0800

    RPS game with js and css

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,6 +1,24 @@
 // getComputerChoice() - computer to randomly return either rock/paper/scissors.
-// playRound() - takes 2 parameter playerSelection and computerSelection and declares the winner.
+// playRound() - takes 2 parameter playerSelection and computerSelection and declares the winner either player/computer/0.
 // game() - call playRound() and play 5 rounds and reports the winner or loser at the end.
+
+let player_score = 0;
+let computer_score = 0;
+let tie_score = 0;
+let rounds = 0;
+
+const player = document.querySelector(".playerScore");
+const computer = document.querySelector(".computerScore");
+const tie = document.querySelector(".tie");
+const choices = document.querySelectorAll(".choice");
+const result = document.querySelector(".result");
+const playerImage = document.querySelector(".playerChoice");
+const computerImage = document.querySelector(".computerChoice");
+const playerWindow = document.querySelector(".player");
+const computerWindow = document.querySelector(".computer");
+
+const pImage = document.createElement("img");
+const cImage = document.createElement("img");
 
 function getComputerChoice() {
   let choice = ["rock", "paper", "scissors"];
@@ -25,56 +43,122 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  let computer_score = 0;
-  let player_score = 0;
-  let tie = 0;
+function update(winner) {
+  if (winner == "player") {
+    player_score++;
+    player.textContent = player_score;
+  } else if (winner == "computer") {
+    computer_score++;
+    computer.textContent = computer_score;
+  } else {
+    tie_score++;
+    tie.textContent = tie_score;
+  }
+}
 
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = "";
-    do {
-      playerSelection = prompt("Rock, Paper, Scissors.  What is your choice ?");
-    } while (
-      playerSelection.toLowerCase() != "rock" &&
-      playerSelection.toLowerCase() != "paper" &&
-      playerSelection.toLowerCase() != "scissors"
-    );
-    let computerSelection = getComputerChoice();
-
-    let winner = playRound(playerSelection, computerSelection);
-
-    if (winner == "player") {
-      player_score++;
-      alert(`You chose ${playerSelection}`);
-      alert(`Computer chose ${computerSelection}`);
-      alert("You won this round !");
-    }
-    if (winner == "computer") {
-      computer_score++;
-      alert(`You chose ${playerSelection}`);
-      alert(`Computer chose ${computerSelection}`);
-      alert("Computer won this round !");
-    }
-    if (winner == 0) {
-      tie++;
-      alert(`You chose ${playerSelection}`);
-      alert(`Computer chose ${computerSelection}`);
-      alert(`This round is a tie !`);
-    }
+// Define a function to handle clicks on the choices
+function handleChoiceClick(choice) {
+  if (rounds > 4) {
+    //printWinner();
+    // Remove the click event listener from each choice
+    choices.forEach((choice) => {
+      choice.removeEventListener("click", handleChoiceClick);
+    });
+    return 0;
   }
 
-  if (player_score > computer_score) {
-    alert(
-      `Your score : ${player_score},  Computer's score : ${computer_score}, Tie : ${tie}\n You Won!`
-    );
-  } else if (player_score < computer_score) {
-    alert(
-      `Your score : ${player_score},  Computer's score : ${computer_score}, Tie : ${tie}\n You Lost!`
-    );
+  playerChoice = choice.getAttribute("data-choice");
+  computerChoice = getComputerChoice();
+
+  updateImage(playerChoice, computerChoice);
+  winner = playRound(playerChoice, computerChoice);
+  update(winner);
+  resizing(winner);
+
+  rounds++;
+
+  printWinner();
+}
+
+function game() {
+  // Add the click event listener to each choice
+  choices.forEach((choice) => {
+    choice.addEventListener("click", () => handleChoiceClick(choice));
+  });
+}
+
+function printWinner() {
+  if (rounds == 5) {
+    if (player_score > computer_score) {
+      result.textContent = "You Won !";
+    } else if (computer_score > player_score) {
+      result.textContent = "You Lost !";
+    } else {
+      result.textContent = "Its a Tie !";
+    }
   } else {
-    alert(
-      `Your score : ${player_score},  Computer's score : ${computer_score}, Tie : ${tie}\n Its a tie!`
-    );
+    return 0;
+  }
+}
+
+function updateImage(playerSelection, computerSelection) {
+  pImage.classList.add("imagePlacement");
+  cImage.classList.add("imagePlacement");
+
+  // Update player image
+  if (playerSelection == "rock") {
+    pImage.src = "images/rock.png";
+    pImage.alt = "rock";
+  } else if (playerSelection == "paper") {
+    pImage.src = "images/paper.png";
+    pImage.alt = "paper";
+  } else if (playerSelection == "scissors") {
+    pImage.src = "images/scissors.png";
+    pImage.alt = "scissors";
+  }
+
+  // Update computer image
+  if (computerSelection == "rock") {
+    cImage.src = "images/rock.png";
+    cImage.alt = "rock";
+  } else if (computerSelection == "paper") {
+    cImage.src = "images/paper.png";
+    cImage.alt = "paper";
+  } else if (computerSelection == "scissors") {
+    cImage.src = "images/scissors.png";
+    cImage.alt = "scissors";
+  }
+
+  if (rounds > 0) {
+    playerImage.removeChild(pImage);
+    computerImage.removeChild(cImage);
+  }
+
+  playerImage.appendChild(pImage);
+  computerImage.appendChild(cImage);
+}
+
+// Resizing the window to display winner and loser.  
+function resizing(winner) {
+  if (playerWindow.classList.contains("win")) {
+    playerWindow.classList.remove("win");
+  }
+  if (playerWindow.classList.contains("lose")) {
+    playerWindow.classList.remove("lose");
+  }
+  if (computerWindow.classList.contains("win")) {
+    computerWindow.classList.remove("win");
+  }
+  if (computerWindow.classList.contains("lose")) {
+    computerWindow.classList.remove("lose");
+  }
+
+  if (winner == "player") {
+    playerWindow.classList.add("win");
+    computerWindow.classList.add("lose");
+  } else if (winner == "computer") {
+    playerWindow.classList.add("lose");
+    computerWindow.classList.add("win");
   }
 }
 
